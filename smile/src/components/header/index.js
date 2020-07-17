46b440f114383e6c356e7f683a45334e23be5db1
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-07-17 18:04:03 +0300

    feat: finished header component

--- smile/src/components/header/index.js
+++ smile/src/components/header/index.js
@@ -1,37 +1,23 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import Logo from '../logo';
+import Input from '../input';
+import { Link } from 'react-router-dom';
 import styles from './index.module.css';
 import homeIcon from '../../images/home-run.svg';
 import heartIcon from '../../images/heart.svg';
 import userIcon from '../../images/user.svg';
-import arrow from '../../images/next.svg';
 
 const Header = () => {
-    const history = useHistory();
-
-    const routeChange = () => {
-        history.push("/search");
-    };
-
     return (
         <header>
             <nav>
-                <div className={styles.logo}>
-                    <a href="/"><h1>Smile</h1></a>
-                </div>
-                <div className={styles["input-container"]}>
-                    <input className={styles["custom-input"]} type="text" placeholder="Search" />
-                    <button onClick={routeChange} className={styles["custom-input-button"]}>
-                        <img src={arrow} />
-                    </button>
-                </div>
-                <div className={styles["nav-icons"]}>
-                    <ul>
-                        <li><img className={styles.icon} src={homeIcon} alt="home" /></li>
-                        <li><img className={styles.icon} src={heartIcon} alt="home" /></li>
-                        <li><img className={styles.icon} src={userIcon} alt="home" /></li>
-                    </ul>
-                </div>
+                <Logo />
+                <Input type="text" placeholder="Search" button/>
+                <ul className={styles["nav-icons"]}>
+                    <li><Link to="/"><img className={styles.icon} src={homeIcon} alt="home" /></Link></li>
+                    <li><Link to="/requests"><img className={styles.icon} src={heartIcon} alt="heart" /></Link></li>
+                    <li><Link to="/user"><img className={styles.icon} src={userIcon} alt="profile" /></Link></li>
+                </ul>
             </nav>
         </header >
     );
