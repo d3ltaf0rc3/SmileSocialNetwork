3098d7266a3bbc3e3abd27f69752c058618a1150
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-07-17 19:02:39 +0300

    feat: finished footer component

--- smile/src/components/footer/index.js
+++ smile/src/components/footer/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './index.module.css';
 
 const Footer = () => {
     return (
         <footer>
-            
+            <h1 className={styles.copyright}>&copy; 2020 Smile</h1>
+            <Link to="/about" className={styles["about-link"]}>About</Link>
         </footer>
     );
 };
