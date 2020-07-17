2f3de5e8ee327418d7ebd1dd327f35c6c0f4ae5f
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-07-17 20:00:39 +0300

    feat: added routing

--- smile/src/Navigation.js
+++ smile/src/Navigation.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomePage from './components/home';
+import HomePage from './components/home-page';
+import LoginPage from './components/login-page';
+import RegisterPage from './components/register-page';
 
 const Navigation = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={HomePage}/>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage}/>
       </Switch>
     </BrowserRouter>
   );
