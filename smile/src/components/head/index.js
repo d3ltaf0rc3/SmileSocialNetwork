35b48d2291b6c2888b956b6cdddf8446bddd0081
Martin Marinov <sunrise_martin@hotmail.com>
2020-07-20 14:27:56 +0300

    feat: added head component

---
+++ smile/src/components/head/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Helmet } from 'react-helmet';
+
+const Head = (props) => {
+    return (
+        <Helmet>
+            <meta name="description" content="Smile is a Instagram-like social network. You can create a profile and share your pictures with your friends!"/>
+            <title>{props.title}</title>
+        </Helmet>
+    )
+};
+
+export default Head;
