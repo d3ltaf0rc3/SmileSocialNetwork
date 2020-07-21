590b9fd29387aaf3cf2a4dcecc2b475ce5dbd5b5
Martin Marinov <sunrise_martin@hotmail.com>
2020-07-21 19:34:28 +0300

    feat: post card finished

--- smile/src/components/post-card/header/index.js
+++ smile/src/components/post-card/header/index.js
@@ -7,7 +7,7 @@ const PostHeader = (props) => {
     return (
         <div className={styles["post-header"]}>
             <div className={styles["image-container"]}>
-                <Link to='/profile'><Avatar size="32" imageUrl="https://scontent.fpdv1-1.fna.fbcdn.net/v/t31.0-8/27503775_1180394138762784_345829292824939629_o.jpg?_nc_cat=110&_nc_sid=0debeb&_nc_ohc=gIYKPmMFfEMAX-y__Gh&_nc_ht=scontent.fpdv1-1.fna&oh=bfe878ff5e818d3010003b82e0d00fa5&oe=5F3A0C5F" /></Link>
+                <Link to='/profile'><Avatar size="32" imageUrl="https://scontent.fpdv1-1.fna.fbcdn.net/v/t31.0-8/28071297_1190906327711565_6994321913720957204_o.jpg?_nc_cat=109&_nc_sid=0debeb&_nc_ohc=1bE1Jv1JChQAX9xgl8D&_nc_ht=scontent.fpdv1-1.fna&oh=ac4f57f5d742ed3b644fed6fc8f11c67&oe=5F3AD470" /></Link>
             </div>
             <div>
                 <span className={styles["post-author"]}><Link className={styles.link} to="/user/lmm_47">lmm_47</Link></span>
--- smile/src/components/post-card/index.js
+++ smile/src/components/post-card/index.js
@@ -9,7 +9,7 @@ const Post = (props) => {
     return (
         <div className={styles.card}>
             <PostHeader />
-            <img className={styles["post-image"]} src="https://scontent.fpdv1-1.fna.fbcdn.net/v/t31.0-8/27503775_1180394138762784_345829292824939629_o.jpg?_nc_cat=110&_nc_sid=0debeb&_nc_ohc=gIYKPmMFfEMAX-y__Gh&_nc_ht=scontent.fpdv1-1.fna&oh=bfe878ff5e818d3010003b82e0d00fa5&oe=5F3A0C5F" alt="post" />
+            <img className={styles["post-image"]} src={props.imageUrl} alt="post" />
             <div className={styles["post-info"]}>
                 <PostActions />
                 <span className={styles["post-likes"]}>147 likes</span>
