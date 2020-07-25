df620036cdbedaa9277a77dc30eadd537c4d1714
Martin Marinov <sunrise_martin@hotmail.com>
2020-07-25 13:10:03 +0300

    feat: auth function changed

--- REST-API/utils/auth.js
+++ REST-API/utils/auth.js
@@ -1,27 +1,14 @@
 const jwt = require('./jwt');
-const User = require('../models/User');
 
-module.exports = (redirectAuthenticated = true) => {
-    return function (req, res, next) {
-        const token = req.cookies[config.authCookieName] || 'auth-token';
+module.exports = (req, res, next) => {
+    const token = req.cookies['auth-token'];
 
-        jwt.verifyToken(token)
-            .then(data => {
-                User.findById(data.id)
-                    .then((user) => {
-                        req.user = user;
-                        next();
-                    });
-            })
-            .catch(err => {
-                if (!redirectAuthenticated) { next(); return; }
-
-                if (['token expired', 'blacklisted token', 'jwt must be provided'].includes(err.message)) {
-                    res.status(401).send('UNAUTHORIZED!');
-                    return;
-                }
-
-                next(err);
-            });
-    };
+    try {
+        jwt.verify(token, process.env.JWT_KEY);
+        next();
+    } catch (error) {
+        return res.status(401).send({
+            error: "User not authenticated!"
+        });
+    }
 };
