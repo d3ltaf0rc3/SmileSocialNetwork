b4e35751d4c1f4ffd04c412891e857865b3b2a3b
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-11-17 17:54:21 +0200

    fix: login bug

--- REST-API/models/User.js
+++ REST-API/models/User.js
@@ -12,8 +12,7 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        select: false
+        required: true
     },
     profilePicture: {
         type: String,
