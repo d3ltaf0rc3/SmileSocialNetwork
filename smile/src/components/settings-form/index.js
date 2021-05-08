55877d4cb83128352c7fe291120f31659176f2ff
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-05-08 23:08:22 +0300

    settings page refactoring and minor changes

--- smile/src/components/settings-form/index.js
+++ smile/src/components/settings-form/index.js
@@ -1,39 +1,23 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import styles from './index.module.css';
-import { Link, withRouter } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Input from '../input';
 import Textarea from '../textarea';
+import editUser from '../../utils/editUser';
 import UserContext from '../../contexts/AuthContext';
 
-const SettingsForm = (props) => {
+const SettingsForm = () => {
     const context = useContext(UserContext);
+    const history = useHistory();
     const [name, setName] = useState(context.user.name);
     const [bio, setBio] = useState(context.user.description);
     const [isPrivate, setIsPrivate] = useState(context.user.isPrivate);
 
-    const updateUser = (e) => {
-        e.preventDefault();
-
+    const updateUser = () => {
         if (name !== context.user.name ||
             bio !== context.user.description ||
             isPrivate !== context.user.isPrivate) {
-            fetch(`${process.env.REACT_APP_API_URL}/api/user/edit`, {
-                method: "put",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                credentials: "include",
-                body: JSON.stringify({
-                    name,
-                    description: bio,
-                    isPrivate
-                })
-            })
-                .then(() => {
-                    context.triggerUpdate();
-                    props.history.push(`/user/${context.user.username}`);
-                })
-                .catch(err => console.error(err));
+            editUser({ name, description: bio, isPrivate }, context, history);
         }
     };
 
@@ -41,11 +25,11 @@ const SettingsForm = (props) => {
         <form className={styles.form}>
             <div className={styles["input-container"]}>
                 <label>Name</label>
-                <Input value={props.name} onChange={(e) => setName(e.target.value)} placeholder="Name" type="text" />
+                <Input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" type="text" />
             </div>
             <div className={styles["input-container"]}>
                 <label>Bio</label>
-                <Textarea onChange={(e) => setBio(e.target.value)} value={props.bio} placeholder="Bio" />
+                <Textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder="Bio" />
             </div>
 
             <div className={styles["switch-box"]}>
@@ -53,18 +37,19 @@ const SettingsForm = (props) => {
                 <input
                     className={styles.switch}
                     onChange={(e) => setIsPrivate(e.target.checked)}
-                    defaultChecked={props.isPrivate}
+                    defaultChecked={isPrivate}
                     id="private-profile-checkbox"
-                    type="checkbox" />
+                    type="checkbox"
+                />
             </div>
 
             <div className={styles.actions}>
-                <button onClick={updateUser}>Save changes</button>
-                <Link to={`/user/${props.username}`}>Cancel</Link>
+                <button type="button" onClick={updateUser}>Save changes</button>
+                <Link to={`/user/${context.user.username}`}>Cancel</Link>
                 <Link to="/change-password">Change password</Link>
             </div>
         </form>
     )
 };
 
-export default withRouter(SettingsForm);
+export default SettingsForm;
--- smile/src/pages/settings-page/index.js
+++ smile/src/pages/settings-page/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react';
+import { Fragment, useContext } from 'react';
 import styles from './index.module.css';
 import Head from '../../components/head';
 import Header from '../../components/header';
@@ -6,10 +6,12 @@ import Footer from '../../components/footer';
 import SettingsForm from '../../components/settings-form';
 import UserData from '../../components/user-data';
 import UserContext from '../../contexts/AuthContext';
-import { withRouter } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import editUser from '../../utils/editUser';
+import Loading from '../../components/loading-spinner';
 
-const SettingsPage = (props) => {
+const SettingsPage = () => {
+    const history = useHistory();
     const context = useContext(UserContext);
     const defaultUrl = "https://res.cloudinary.com/smile-social-network/image/upload/v1600976280/download_udtdbe.png";
 
@@ -32,7 +34,7 @@ const SettingsPage = (props) => {
                         })
                     });
                 }
-                editUser({ profilePicture: result.info.secure_url, public_id: result.info.public_id }, context, props.history);
+                editUser({ profilePicture: result.info.secure_url, public_id: result.info.public_id }, context, history);
             } else if (error) {
                 console.error(error);
             }
@@ -53,32 +55,37 @@ const SettingsPage = (props) => {
                     url: context.user.profilePicture
                 })
             });
-            editUser({ profilePicture: defaultUrl, public_id: null }, context, props.history);
+            editUser({ profilePicture: defaultUrl, public_id: null }, context, history);
         }
     };
+
+    if (!context.user) {
+        return (
+            <div style={{ width: "100vw", height: "calc(100vh - 300px)", display: "grid", placeItems: "center" }}>
+                <Loading />
+            </div>
+        )
+    }
 
     return (
         <Fragment>
             <Head title="Settings | Smile" />
             <Header />
-            {!context.user ? null :
-                <div className={styles["form-container"]}>
-                    <div className={styles["avatar-actions"]}>
-                        <UserData imageUrl={context.user.profilePicture} username={context.user.username} />
-                        <div className={styles.buttons}>
-                            <button className={styles.btn} onClick={openWidget}>Change photo</button>
-                            {context.user.profilePicture !== defaultUrl ? <button className={styles.btn} onClick={removeProfilePic}>Remove photo</button> : null}
-                        </div>
+            <div className={styles["form-container"]}>
+                <div className={styles["avatar-actions"]}>
+                    <UserData imageUrl={context.user.profilePicture} username={context.user.username} />
+                    <div className={styles.buttons}>
+                        <button className={styles.btn} onClick={openWidget}>Change photo</button>
+                        {context.user.profilePicture !== defaultUrl ? (
+                            <button className={styles.btn} onClick={removeProfilePic}>Remove photo</button>
+                        ) : null}
                     </div>
-                    <SettingsForm
-                        username={context.user.username}
-                        name={context.user.name}
-                        bio={context.user.description}
-                        isPrivate={context.user.isPrivate} />
-                </div>}
+                </div>
+                <SettingsForm />
+            </div>
             <Footer />
         </Fragment>
     )
 };
 
-export default withRouter(SettingsPage);
+export default SettingsPage;
