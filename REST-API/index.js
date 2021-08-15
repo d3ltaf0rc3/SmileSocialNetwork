50faa5920d62f534704fa7c32408b88fd1fb925b
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-08-15 20:04:34 +0300

    remove error function

--- REST-API/index.js
+++ REST-API/index.js
@@ -9,7 +9,6 @@ require("./config/database")();
 app.use("/api/posts", postRouter);
 app.use("/api/user", userRouter);
 
-app.listen(process.env.PORT, err => {
-    if (err) throw err;
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
