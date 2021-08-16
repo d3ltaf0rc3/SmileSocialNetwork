9b2bfbc993c4cead2380217e09b2a3d4b9cb643e
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-08-16 22:44:59 +0300

    change recaptcha verification function

--- REST-API/utils/verifyReCaptcha.js
+++ REST-API/utils/verifyReCaptcha.js
@@ -1,11 +1,11 @@
-async function verifyReCaptcha(req, res) {
-    require("es6-promise").polyfill();
-    require("isomorphic-fetch");
+const fetch = require("node-fetch");
+const response = require("./responseGenerator");
 
+async function verifyReCaptcha(req, res) {
     const RECAPTCHA_SERVER_KEY = process.env.RECAPTCHA_SERVER_KEY;
     const humanKey = req.body.key;
 
-    const isHuman = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+    const isHuman = await fetch("https://www.google.com/recaptcha/api/siteverify", {
         method: "post",
         headers: {
             Accept: "application/json",
@@ -16,14 +16,14 @@ async function verifyReCaptcha(req, res) {
         .then(res => res.json())
         .then(json => json.success)
         .catch(err => {
-            return res.status(503).send(`Error in Google Siteverify API. ${err.message}`);
+            return res.status(503).send(response("fail", `Error in Google Siteverify API. ${err.message}`));
         });
 
     if (humanKey === null || !isHuman) {
-        return res.status(401).send("YOU ARE NOT A HUMAN");
+        return res.status(401).send(response("fail", "YOU ARE NOT A HUMAN"));
     }
 
-    return res.status(204).send();
+    return res.send(response("success", "ReCaptcha verified successfully!"));
 }
 
 module.exports = verifyReCaptcha;
