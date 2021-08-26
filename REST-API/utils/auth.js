404606725296bf561f297ff9aa16673bbec0ddfa
Martin Marinov <sunrise_martin@hotmail.com>
2021-08-26 23:18:52 +0300

    refactoring of utils

--- REST-API/utils/auth.js
+++ REST-API/utils/auth.js
@@ -3,6 +3,10 @@ const response = require("./responseGenerator");
 
 module.exports = (req, res, next) => {
   try {
+    if (!req.cookies["auth-token"]) {
+      return res.status(401).send(response("fail", "Missing auth cookie!"));
+    }
+
     const id = jwt.verify(req.cookies["auth-token"], process.env.JWT_KEY);
     req.userId = id;
     next();
--- REST-API/utils/deleteSensitiveData.js
+++ REST-API/utils/deleteSensitiveData.js
@@ -1,22 +1,13 @@
-const deleteUserSensitiveData = (user) => {
+const deleteSensitiveData = (user) => {
   const copy = { ...user.toObject() };
 
   delete copy.requests;
   delete copy.password;
+  delete copy.followers;
+  delete copy.following;
   delete copy.posts;
 
   return copy;
 };
 
-const deletePostSensitiveData = (post) => {
-  const copy = { ...post.toObject() };
-
-  delete copy.public_id;
-
-  return copy;
-};
-
-module.exports = {
-  deletePostSensitiveData,
-  deleteUserSensitiveData,
-};
+module.exports = deleteSensitiveData;
