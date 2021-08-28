94a522a943dd1d5567085300a2dd20240e800274
Martin Marinov <sunrise_martin@hotmail.com>
2021-08-28 19:43:28 +0300

    add contexts

---
+++ smile/contexts/postContext.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PostContext = React.createContext({
+  _id: null,
+  location: null,
+  description: null,
+  likes: null,
+  comments: null,
+  resource: null,
+  public_id: null,
+  resource_type: null,
+  postedBy: null,
+  createdAt: null,
+});
+
+export default PostContext;
---
+++ smile/contexts/profileContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProfileContext = React.createContext({
+  _id: null,
+  name: null,
+  profilePicture: null,
+  public_id: null,
+  description: null,
+  isPrivate: null,
+  username: null,
+  hasRequested: null,
+});
+
+export default ProfileContext;
