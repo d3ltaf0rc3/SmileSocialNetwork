4c4a594ea1af1e2e41811bda3c0d29be627f133a
Martin Marinov <sunrise_martin@hotmail.com>
2021-08-29 10:54:18 +0300

    add _app file

---
+++ smile/pages/_app.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import AuthContext from '../contexts/authContext';
+import Spinner from '../components/loading-spinner';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => {
+  const [user, setUser] = useState();
+
+  const logIn = (usr) => {
+    setUser(usr);
+  };
+
+  const logOut = () => {
+    setUser(null);
+  };
+
+  useEffect(() => {
+    fetch(`${window.location.origin}/api/user/verifySession`, {
+      method: 'get',
+      credentials: 'include',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.success) {
+          logIn(res.data);
+        } else {
+          logOut();
+        }
+      })
+      .catch(() => logOut());
+  }, []);
+
+  if (user === undefined) {
+    return <Spinner />;
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        logIn,
+        logOut,
+      }}
+    >
+      <Head>
+        <title>Smile</title>
+      </Head>
+      <Component {...pageProps} />
+    </AuthContext.Provider>
+  );
+};
+
+export default MyApp;
