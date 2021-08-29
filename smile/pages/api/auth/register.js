100c21370a7dfcac3b6ef5df6804579a23701a02
Martin Marinov <sunrise_martin@hotmail.com>
2021-08-29 22:03:27 +0300

    login and register api routes

--- smile/pages/api/auth/register.js
+++ smile/pages/api/auth/register.js
@@ -1,3 +1,6 @@
+import { serialize } from 'cookie';
+import options from '../../../utils/cookieOptions';
+
 export default async function handler(req, res) {
   try {
     const resp = await fetch(`${process.env.API_URL}/api/user/register`, {
@@ -8,6 +11,9 @@ export default async function handler(req, res) {
       body: JSON.stringify(req.body),
     });
     const data = await resp.json();
+    if (resp.status === 201) {
+      res.setHeader('Set-Cookie', serialize('auth-token', data.data.token, options));
+    }
     return res.status(resp.status).send(data);
   } catch (error) {
     return res.status(500).send({
