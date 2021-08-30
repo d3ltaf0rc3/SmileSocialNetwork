9b1eb46a769ee04c99dfd2404d8d791de87e796c
Martin Marinov <sunrise_martin@hotmail.com>
2021-08-30 15:29:31 +0300

    change cookie options

--- smile/utils/cookieOptions.js
+++ smile/utils/cookieOptions.js
@@ -1,14 +1,9 @@
-const prodOptions = {
-  expires: new Date(Date.now() + 604800000),
-  sameSite: 'None',
-  secure: true,
-  path: '/',
-};
-
-const devOptions = {
+const cookieOptions = {
   expires: new Date(Date.now() + 604800000),
   httpOnly: true,
+  sameSite: 'Strict',
   path: '/',
+  secure: process.env.NODE_ENV === 'production',
 };
 
-export default process.env.NODE_ENV === 'production' ? prodOptions : devOptions;
+export default cookieOptions;
