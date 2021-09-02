e23d3c1b9daf257838235e36de324c65f6e6596c
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-09-02 22:11:02 +0300

    send null if no cookie is present

--- smile/pages/api/user/verifySession.js
+++ smile/pages/api/user/verifySession.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const resp = await fetch(`${process.env.API_URL}/api/user/verify/session`, {
       method: 'get',
       headers: {
-        Authorization: req.cookies['auth-token'],
+        Authorization: req.cookies['auth-token'] || null,
       },
     });
     const data = await resp.json();
