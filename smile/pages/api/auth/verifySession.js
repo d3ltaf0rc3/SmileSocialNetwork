0135e09265663d8d6b8e27b6b60bf84ae67643f8
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-09-18 20:17:03 +0300

    clear cookie if session has expired

--- smile/pages/api/auth/verifySession.js
+++ smile/pages/api/auth/verifySession.js
@@ -1,3 +1,5 @@
+import { serialize } from 'cookie';
+
 export default async function handler(req, res) {
   try {
     const resp = await fetch(`${process.env.API_URL}/api/user/verify/session`, {
@@ -7,6 +9,9 @@ export default async function handler(req, res) {
       },
     });
     const data = await resp.json();
+    if (resp.status !== 200) {
+      res.setHeader('Set-Cookie', serialize('auth-token', '', { maxAge: -1, path: '/' }));
+    }
     return res.status(resp.status).send(data);
   } catch (error) {
     return res.status(500).send({
