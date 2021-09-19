4cf0a3ffb6ea81ab9fd2f60c32b2aca7405e25e8
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-09-19 16:47:21 +0300

    initialize sentry

--- REST-API/index.js
+++ REST-API/index.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 const postRouter = require("./routes/post");
 const userRouter = require("./routes/user");
 const app = express();
+
+Sentry.init({
+  environment: process.env.NODE_ENV === "production" ? "production" : "development",
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+})
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 require("./config/express")(app);
 require("./config/database")();
 
 app.use("/api/posts", postRouter);
 app.use("/api/user", userRouter);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
