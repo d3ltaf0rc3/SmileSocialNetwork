bebd5304dcfc488cf0acb886d7ec8a21327ff155
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-09-30 10:33:37 +0300

    add route guard function

---
+++ smile/utils/requirePageAuth.js
@@ -0,0 +1,30 @@
+export default async function requirePageAuth({ req }) {
+  const user = await fetch(`${process.env.API_URL}/api/user/verify/session`, {
+    method: 'get',
+    headers: {
+      Authorization: req.cookies['auth-token'] || null,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((resp) => {
+      if (resp.success) {
+        return resp.data;
+      }
+      return null;
+    })
+    .catch(() => null);
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      user,
+    },
+  };
+}
