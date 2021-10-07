98802cd9364f88177ee3ef660729fbf424b945e0
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-10-07 17:05:48 +0300

    user profile page

--- smile/pages/user/[username].jsx
+++ smile/pages/user/[username].jsx
@@ -1,65 +1,36 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState, useContext } from 'react';
+import Head from 'next/head';
 import Header from '../../components/header';
-import Footer from '../../components/footer';
 import ProfileHeader from '../../components/profile-components/header';
-import PhotosGrid from '../../components/profile-components/photos-grid';
-import Private from '../../components/profile-components/private-profile';
-import ProfileContext from '../../contexts/profileContext';
-import UserContext from '../../contexts/authContext';
-import Spinner from '../../components/loading/spinner';
-import NoPosts from '../../components/profile-components/no-posts';
+import PostsSection from '../../components/profile-components/posts';
+import Footer from '../../components/footer';
+import AuthContext from '../../contexts/authContext';
+import requirePageAuth from '../../utils/requirePageAuth';
 import styles from '../../styles/profile.module.css';
 
-const ProfilePage = () => {
+const ProfilePage = ({ user }) => {
   const router = useRouter();
   const { username } = router.query;
-  const { user } = useContext(UserContext);
-  const [profile, setProfile] = useState(null);
-  const [didUpdate, setUpdate] = useState();
-
-  useEffect(() => {
-    fetch(`${window.location.origin}/api/user/getProfile?username=${username}`, {
-      method: 'get',
-      credentials: 'include',
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        if (typeof res === 'object') {
-          setProfile(res);
-        } else {
-          router.push('/error');
-        }
-      })
-      .catch(() => router.push('/error'));
-  }, [router, username, didUpdate]);
-
-  if (profile === null || user === null) {
-    return <Spinner withWrapper />;
-  }
-
-  const hasProfileAccess =
-    user.username === profile.username ||
-    profile.followers.some((usr) => usr.username === user.username) ||
-    !profile.isPrivate;
-  const hasPosts = profile.posts.length === 0 ? <NoPosts /> : <PhotosGrid />;
 
   return (
-    <ProfileContext.Provider
+    <AuthContext.Provider
       value={{
-        ...profile,
-        doesUserFollow: profile.followers.some((usr) => usr.username === user.username),
-        triggerUpdate: () => setUpdate(!didUpdate),
+        user,
       }}
     >
+      <Head>
+        <title>@{username} | Smile</title>
+      </Head>
       <Header />
       <div className={styles.container}>
-        <ProfileHeader />
-        {hasProfileAccess ? hasPosts : <Private />}
+        <ProfileHeader username={username} />
+        <PostsSection username={username} />
       </div>
       <Footer />
-    </ProfileContext.Provider>
+    </AuthContext.Provider>
   );
 };
 
 export default ProfilePage;
+
+export const getServerSideProps = requirePageAuth;
