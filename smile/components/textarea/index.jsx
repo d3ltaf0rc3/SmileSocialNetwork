66b4d3f3323a492d02af8b0175c9eebec2c95e71
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-10-07 14:51:06 +0300

    textarea component

--- smile/components/textarea/index.jsx
+++ smile/components/textarea/index.jsx
@@ -2,9 +2,12 @@ import styles from './index.module.css';
 
 const Textarea = ({ value, onChange, placeholder }) => {
   return (
-    <div className={styles.container}>
-      <textarea defaultValue={value} onChange={onChange} placeholder={placeholder} />
-    </div>
+    <textarea
+      className={styles.textarea}
+      defaultValue={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
   );
 };
 
