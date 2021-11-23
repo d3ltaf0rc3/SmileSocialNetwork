156cf267af2185ca03ecd6db051b40c39a114157
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-11-23 19:02:34 +0200

    remove header and footer

--- smile/pages/404.jsx
+++ smile/pages/404.jsx
@@ -1,27 +1,17 @@
-import { useContext } from 'react';
 import Link from 'next/link';
-import Header from '../components/header';
-import Footer from '../components/footer';
-import AuthContext from '../contexts/authContext';
 import styles from '../styles/error.module.css';
 
 const ErrorPage = () => {
-  const { user } = useContext(AuthContext);
-
   return (
-    <>
-      {user ? <Header /> : null}
-      <div className={styles.container}>
-        <h1>Sorry, this page isn&apos;t available.</h1>
-        <p>
-          The link you followed may be broken, or the page may have been removed.{' '}
-          <Link href={user ? '/' : '/login'}>
-            <a className={styles.link}>Go back to Smile.</a>
-          </Link>
-        </p>
-      </div>
-      {user ? <Footer /> : null}
-    </>
+    <div className={styles.container}>
+      <h1>Sorry, this page isn&apos;t available.</h1>
+      <p>
+        The link you followed may be broken, or the page may have been removed.{' '}
+        <Link href="/">
+          <a className={styles.link}>Go back to Smile.</a>
+        </Link>
+      </p>
+    </div>
   );
 };
 
