fd218d7ed693f698e9d1c04adbe146244dc0fb57
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-11-27 21:24:08 +0200

    implement authorization header validation

--- REST-API/controllers/session.js
+++ REST-API/controllers/session.js
@@ -6,13 +6,24 @@ const response = require("../utils/responseGenerator");
 const deleteSensitiveData = require("../utils/deleteSensitiveData");
 
 async function verifySession(req, res) {
-  if (!req.headers.authorization || req.headers.authorization === "null") {
+  if (!req.headers.authorization) {
     return res.status(401).send(response("fail", "Missing authorization!"));
   }
 
+  const authHeader = req.headers.authorization.split(" ");
+
+  if (authHeader.length !== 2) {
+    return res.status(400).send(response("fail", "Invalid authorization header format!"));
+  } else if (authHeader[0] !== 'Bearer') {
+    return res.status(400).send(response("fail", "Unsupported auth format!"));
+  } else if (!authHeader[1] || authHeader[1] === "null") {
+    return res.status(400).send(response("fail", "Missing authorization!"));
+  }
+  const token = authHeader[1];
+
   try {
-    const { id } = jwt.verify(req.headers.authorization, process.env.JWT_KEY);
-    const session = await Session.findOne({ token: req.headers.authorization });
+    const { id } = jwt.verify(token, process.env.JWT_KEY);
+    const session = await Session.findOne({ token });
 
     if (session === null) {
       return res.status(401).send(response("fail", "Session does not exist!"));
@@ -27,6 +38,9 @@ async function verifySession(req, res) {
     const userToSend = deleteSensitiveData(user);
     return res.send(response("success", userToSend));
   } catch (error) {
+    if (error.message === "invalid signature" || error.message === "jwt malformed") {
+      return res.status(401).send(response("fail", "Malformed JWT token!"))
+    }
     Sentry.captureException(error);
     res.status(500).send(response("fail", error.message));
   }
--- REST-API/utils/auth.js
+++ REST-API/utils/auth.js
@@ -5,13 +5,24 @@ const User = require("../models/User");
 const response = require("./responseGenerator");
 
 module.exports = async (req, res, next) => {
-  if (!req.headers.authorization || req.headers.authorization === "null") {
+  if (!req.headers.authorization) {
     return res.status(401).send(response("fail", "Missing authorization!"));
   }
 
+  const authHeader = req.headers.authorization.split(" ");
+
+  if (authHeader.length !== 2) {
+    return res.status(400).send(response("fail", "Invalid authorization header format!"));
+  } else if (authHeader[0] !== 'Bearer') {
+    return res.status(400).send(response("fail", "Unsupported auth format!"));
+  } else if (!authHeader[1] || authHeader[1] === "null") {
+    return res.status(400).send(response("fail", "Missing authorization!"));
+  }
+  const token = authHeader[1];
+
   try {
-    const { id } = jwt.verify(req.headers.authorization, process.env.JWT_KEY);
-    const session = await Session.findOne({ token: req.headers.authorization });
+    const { id } = jwt.verify(token, process.env.JWT_KEY);
+    const session = await Session.findOne({ token });
 
     if (session === null) {
       return res.status(401).send(response("fail", "Invalid session!"));
@@ -25,6 +36,9 @@ module.exports = async (req, res, next) => {
     req.sessionId = session.id;
     next();
   } catch (error) {
+    if (error.message === "invalid signature" || error.message === "jwt malformed") {
+      return res.status(401).send(response("fail", "Malformed JWT token!"))
+    }
     Sentry.captureException(error);
     return res.status(401).send(response("fail", "Unauthorized!"));
   }
