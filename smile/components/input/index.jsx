c9ca7205649e4e5f12339610af112dcd810ff651
Martin Marinov <sunrise_martin@hotmail.com>
2022-01-01 20:44:54 +0200

    add onFocus and change defaultValue to value

--- smile/components/input/index.jsx
+++ smile/components/input/index.jsx
@@ -1,16 +1,17 @@
 import styles from './index.module.css';
 
-const Input = ({ name, value, onChange, type, placeholder }) => {
+const Input = ({ name, value, onChange, onFocus, type = 'text', placeholder }) => {
   return (
     <div className={styles.container}>
       <input
         className={styles.input}
         name={name}
         autoComplete="off"
-        defaultValue={value}
+        value={value}
         onChange={onChange}
         type={type}
         placeholder={placeholder}
+        onFocus={onFocus}
       />
     </div>
   );
