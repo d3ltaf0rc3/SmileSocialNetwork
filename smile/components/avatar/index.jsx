801d2e087428f26e662b5032119eaec4bfcddc6c
Martin Marinov <sunrise_martin@hotmail.com>
2022-01-01 17:28:24 +0200

    add avatar component

---
+++ smile/components/avatar/index.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import styles from './index.module.css';
+
+const Avatar = ({ size, src }) => {
+  return (
+    <div style={{ width: `${size}px`, height: `${size}px` }} className={styles.container}>
+      <Image src={src} width={size} height={size} alt="user profile picture" objectFit="cover" />
+    </div>
+  );
+};
+
+export default Avatar;
