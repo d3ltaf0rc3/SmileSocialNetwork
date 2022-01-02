aa5531eee206687a1926a86ef1ba148b58a48e1c
Martin Marinov <sunrise_martin@hotmail.com>
2022-01-02 22:12:37 +0200

    add changeProfilePicture api route

---
+++ smile/pages/api/user/profilePicture.js
@@ -0,0 +1,22 @@
+import * as Sentry from '@sentry/nextjs';
+
+export default async function handler(req, res) {
+  try {
+    const resp = await fetch(`${process.env.API_URL}/api/user/edit/profile-picture`, {
+      method: 'put',
+      headers: {
+        Authorization: `Bearer ${req.cookies['auth-token']}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body),
+    });
+    const data = await resp.json();
+    return res.status(resp.status).send(data);
+  } catch (error) {
+    Sentry.captureException(error);
+    return res.status(500).send({
+      success: false,
+      data: error.message,
+    });
+  }
+}
