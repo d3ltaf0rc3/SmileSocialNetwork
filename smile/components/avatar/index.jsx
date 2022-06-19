1e44fe38a5c4b339cf80e2eabfeec11658737393
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2022-06-19 13:17:13 +0300

    add quality attribute

--- smile/components/avatar/index.jsx
+++ smile/components/avatar/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.module.css';
 const Avatar = ({ size, src }) => {
   return (
     <div style={{ width: `${size}px`, height: `${size}px` }} className={styles.container}>
-      <Image src={src} width={size} height={size} alt="user profile picture" objectFit="cover" />
+      <Image src={src} width={size} height={size} alt="user profile picture" objectFit="cover" quality={100} />
     </div>
   );
 };
