ab93d9c730f0080b5ff2d48e95b1c822551d7b31
Martin Marinov <sunrise_martin@hotmail.com>
2022-10-15 10:39:40 +0300

    user stats component

--- smile/components/profile-components/stats/index.jsx
+++ smile/components/profile-components/stats/index.jsx
@@ -1,19 +1,19 @@
 import styles from './index.module.css';
 
-const Stat = ({ posts, followers, following }) => {
+const Stats = ({ posts, followers, following }) => {
   return (
-    <ul className={styles.stats}>
-      <li className={styles.stat}>
-        <span className={styles['stat-number']}>{props.posts}</span> posts
-      </li>
-      <li className={styles.stat}>
-        <span className={styles['stat-number']}>{props.followers}</span> followers
-      </li>
-      <li className={styles.stat}>
-        <span className={styles['stat-number']}>{props.following}</span> following
-      </li>
-    </ul>
+    <div className={styles.statContainer}>
+      <div className={styles.stat}>
+        <span className={styles.statNumber}>{posts}</span> posts
+      </div>
+      <div className={styles.stat}>
+        <span className={styles.statNumber}>{followers}</span> followers
+      </div>
+      <div className={styles.stat}>
+        <span className={styles.statNumber}>{following}</span> following
+      </div>
+    </div>
   );
 };
 
-export default Stat;
+export default Stats;
