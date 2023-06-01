4af3e307a359fb09135b7f963f2b15a04bbae2c6
Martin Marinov <sunrise_martin@hotmail.com>
2023-06-01 21:37:23 +0300

    fix link tag

--- smile/pages/404.jsx
+++ smile/pages/404.jsx
@@ -7,8 +7,8 @@ const ErrorPage = () => {
       <h1>Sorry, this page isn&apos;t available.</h1>
       <p>
         The link you followed may be broken, or the page may have been removed.{' '}
-        <Link href="/">
-          <a className={styles.link}>Go back to Smile.</a>
+        <Link className={styles.link} href="/">
+          Go back to Smile.
         </Link>
       </p>
     </div>
