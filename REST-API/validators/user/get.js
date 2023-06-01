f4376ec7083d360adb871f5ab3389f57ea9784d3
Martin Marinov <sunrise_martin@hotmail.com>
2023-06-01 21:29:24 +0300

    change username max length to 32

--- REST-API/validators/user/get.js
+++ REST-API/validators/user/get.js
@@ -2,8 +2,8 @@ const { param } = require("express-validator");
 
 module.exports = [
   param("username", "Username is invalid!")
-    .isLength({ min: 2, max: 18 })
-    .withMessage("Username must be between 2 and 18 characters long!")
+    .isLength({ min: 2, max: 32 })
+    .withMessage("Username must be between 2 and 32 characters long!")
     .matches(/^[\w.]+$/)
     .withMessage("Username can only contain english letters, numbers, underscores and dots!"),
 ];
--- REST-API/validators/user/username.js
+++ REST-API/validators/user/username.js
@@ -5,8 +5,8 @@ module.exports = [
     .trim()
     .not()
     .isEmpty()
-    .isLength({ min: 2, max: 18 })
-    .withMessage("Username must be between 2 and 18 characters long!")
+    .isLength({ min: 2, max: 32 })
+    .withMessage("Username must be between 2 and 32 characters long!")
     .matches(/^[\w.]+$/)
     .withMessage("Username can only contain english letters, numbers, underscores and dots!"),
 ];
